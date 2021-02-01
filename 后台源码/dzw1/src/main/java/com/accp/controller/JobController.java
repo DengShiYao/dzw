ffe8cf0716d0d09d9c8afd02d5ff121389a1355d
JieSiXiao <1253777923@qq.com>
2021-02-01 22:19:50 +0800

    岗位定义控制器

--- 后台源码/dzw1/src/main/java/com/accp/controller/JobController.java
+++ 后台源码/dzw1/src/main/java/com/accp/controller/JobController.java
@@ -1,9 +1,12 @@
 package com.accp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.accp.domain.Job;
+import com.accp.service.IJobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/job")
 public class JobController {
-
+    @Autowired
+    IJobService jobService;
+    @PostMapping
+    public boolean addJob(@PathVariable Job job){
+        return jobService.save(job);
+    }
+    @GetMapping
+    public List<Job> selAll(){
+        System.out.println("进入");
+        return  jobService.list();
+    }
+    @PutMapping
+    public  boolean updateJob(@PathVariable Job job){
+        return  jobService.updateById(job);
+    }
+    @DeleteMapping
+    public boolean removeJob(String jobId){
+        return jobService.removeById(jobId);
+    }
+    @GetMapping("/count")
+    public int getCount(){
+        return jobService.count();
+    }
 }
 
