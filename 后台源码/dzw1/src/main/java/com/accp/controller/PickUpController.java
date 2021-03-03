4722f24189496bae2b182b355c58ce75469f024c
dengsiyao666 <1932543404@qq.com>
2021-03-03 20:56:31 +0800

    小程序接车前天获取到接车的数据

--- 后台源码/dzw1/src/main/java/com/accp/controller/PickUpController.java
+++ 后台源码/dzw1/src/main/java/com/accp/controller/PickUpController.java
@@ -1,9 +1,15 @@
 package com.accp.controller;
 
 
+import com.accp.domain.PickUp;
+import com.accp.service.impl.PickUpServiceImpl;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pickUp")
 public class PickUpController {
+    @Autowired
+    PickUpServiceImpl pickUpService;
 
+    @PostMapping
+    public List<PickUp> findPickUp(){
+        return pickUpService.list();
+    }
+
+    @PostMapping("findPickNum")
+    public Integer findPickNum(){
+        QueryWrapper<PickUp> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("column1","未读");
+        return pickUpService.count(queryWrapper);
+    }
 }
 
